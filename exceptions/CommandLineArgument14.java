474ce5300f281ddab7ee09002165b11af4f34d2e
Sahithya.Vadlamani <Sahithya.Vadlamani@etplin.com>
2024-09-03 11:42:06 +0530

    added development files

--- exceptions/CommandLineArgument14.java
+++
@@ -1,8 +0,0 @@
-package com.evergent.corejava.exceptions;
-
-public class CommandLineArgument14 {
-	public static void main(String[] args) {
-		System.out.println(args[0]);
-		System.out.println(args[1]);
-	}
-}
--- interface1/MyDataInfo.java
+++
@@ -1,6 +0,0 @@
-
-package com.evergent.corejava.interface1;
-
-public interface MyDataInfo {
-public void MyData();
-}
